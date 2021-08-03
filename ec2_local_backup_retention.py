3e8f6b819ab2f191e5e2772cf02cc1445725c29a
Jon Allen <jallen_swrx@yahoo.com>
2021-08-03 16:35:10 -0400

    autopep8  On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   ec2_local_backup_retention.py

--- ec2_local_backup_retention.py
+++ ec2_local_backup_retention.py
@@ -4,115 +4,119 @@
 #
 #  ec2_local_backup_retention.py --days 10 --dir /var/backup  --prefix db_bk.tgz
 #################################################################################
-import sys,os, pytz
+import sys
+import os
+import pytz
 from datetime import datetime, timezone, timedelta
 import argparse
+
 
 def app_run():
     parser = argparse.ArgumentParser(description='EC2 local Backup retention')
     parser.add_argument('--days', help='days to retain ')
-    parser.add_argument('--dir', help='Linux EC2 server dir' )
-    parser.add_argument('--backup_prefix', help="daily backup file prefix - used\'myback\' not \'myback*\'" )
-    parser.add_argument('--dry_run', action="store_true", help='dry-run for testing' )
+    parser.add_argument('--dir', help='Linux EC2 server dir')
+    parser.add_argument(
+        '--backup_prefix', help="daily backup file prefix - used\'myback\' not \'myback*\'")
+    parser.add_argument('--dry_run', action="store_true",
+                        help='dry-run for testing')
     args = parser.parse_args()
     # special arg processing if necessary
-    def check_args():  
+
+    def check_args():
         days_specifed = None
         file_prefix = ""
-        my_dir = ""   
+        my_dir = ""
         dry_run = False
         if (args.dry_run):
             dry_run = True
-        if ( args.days):
+        if (args.days):
             days_specifed = int(args.days)
         else:
             days_specifed = 10
         file_prefix = args.backup_prefix
-        my_dir = args.dir  
+        my_dir = args.dir
         return days_specifed, file_prefix, my_dir, dry_run
     #
     days_specifed, file_prefix, my_dir, dry_run = check_args()
     if my_dir == None:
         print("No dir specified - see -h for commands")
         sys.exit(4)
-        
+
     today = datetime.now(timezone.utc)
-    retention_period = today - timedelta(days = days_specifed ) 
-    
+    retention_period = today - timedelta(days=days_specifed)
+
     # main_entry_point
-    process_ec2_dir(days_specifed, file_prefix, my_dir, dry_run, today, retention_period) 
-    
+    process_ec2_dir(days_specifed, file_prefix, my_dir,
+                    dry_run, today, retention_period)
+
     return
 
+
 def process_ec2_dir(days_specifed, file_prefix, my_dir, dry_run, today, retention_period):
-    
+
     def print_parms(file_prefix, my_dir, today, retention_period):
         print("today's date is ", today)
-        print("Start of retention period (days) ", retention_period )
+        print("Start of retention period (days) ", retention_period)
         print("EC2 server dir:  ", my_dir)
-        print("backup prefix", file_prefix )
+        print("backup prefix", file_prefix)
         return
-    
+
     def delete_files(dry_run, delete_candidate_list):
         for obj in delete_candidate_list:
-            print("Deleting:  ", obj )
-            if ( dry_run == False):
+            print("Deleting:  ", obj)
+            if (dry_run == False):
                 os.remove(obj)
         return
-    
+
     def deletion_summary(delete_candidate_list):
-        if ( len( delete_candidate_list) > 0 ):
-            print ( "Number of files to delete: " , len(delete_candidate_list))
+        if (len(delete_candidate_list) > 0):
+            print("Number of files to delete: ", len(delete_candidate_list))
             print("deleting older files")
         return
-    
+
     def get_dir(my_dir):
-        objects = os.listdir(my_dir) 
+        objects = os.listdir(my_dir)
         os.chdir(my_dir)
-        return objects   
-    
+        return objects
+
     def get_file_timestamp(utc, o):
         o_time = datetime.fromtimestamp(os.stat(o).st_ctime)
         o_time = utc.localize(o_time)
-        return o_time 
-    
+        return o_time
+
     def filter_dir_obj(days_specifed, file_prefix, my_dir, retention_period, filter_lists):
         found_candidate_list = filter_lists[0]
         delete_candidate_list = filter_lists[1]
-        objects = get_dir(my_dir)    
-        utc=pytz.UTC
+        objects = get_dir(my_dir)
+        utc = pytz.UTC
         for o in objects:
             o_time = get_file_timestamp(utc, o)
-            # print("file: ", o, "time: ", o_time ) 
+            # print("file: ", o, "time: ", o_time )
             if o.startswith(file_prefix):
-                print("file: ", o, "time: ", o_time )
+                print("file: ", o, "time: ", o_time)
                 found_candidate_list.append(o)
                 if o_time < retention_period:
-                    print("older than ", days_specifed )
+                    print("older than ", days_specifed)
                     delete_candidate_list.append(o)
         return
-    
+
     def list_summary(found_candidate_list):
         print("***************Summary***************")
         print("Num of objects found:  ", len(found_candidate_list))
         return
-        
+
     delete_candidate_list = []
     found_candidate_list = []
     filter_lists = [delete_candidate_list, found_candidate_list]
-    # main processing loop ec2 files        
+    # main processing loop ec2 files
     print_parms(file_prefix, my_dir, today, retention_period)
-    filter_dir_obj(days_specifed, file_prefix, my_dir, retention_period, filter_lists)  
-    list_summary(found_candidate_list)                       
-    deletion_summary(delete_candidate_list)      
+    filter_dir_obj(days_specifed, file_prefix, my_dir,
+                   retention_period, filter_lists)
+    list_summary(found_candidate_list)
+    deletion_summary(delete_candidate_list)
     delete_files(dry_run, delete_candidate_list)
-    return 
+    return
+
 
 if __name__ == "__main__":
-   app_run()
-
-        
-
-        
-       
-     
+    app_run()
