41645a490843071f342539ea2952609588a64d96
Jon Allen <jallen_swrx@yahoo.com>
2021-08-06 10:17:16 -0400

    updated error processing  Changes to be committed: 	modified:   aws_s3_backup_retention.py

--- aws_s3_backup_retention.py
+++ aws_s3_backup_retention.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python
 #################################################################################
 #  S3 backup daily retenttion job
 #  removes backups older than day specified.
 #
 #  aws_s3_backup_retention --days 10 --bucket testbucket  --prefix db_back.tgz
 #################################################################################
+import sys
 import boto3
+import botocore
 from datetime import datetime, timezone, timedelta
 import pprint
 import argparse
@@ -32,8 +35,16 @@ def app_run():
             days_specifed = int(args.days)
         else:
             days_specifed = 10
-        file_prefix = args.backup_prefix
-        my_bucket = args.bucket
+        if ( args.backup_prefix):
+            file_prefix = args.backup_prefix
+        else:
+             print("Error: No prefix specified")
+             sys.exit(8)
+        if ( args.bucket):
+              my_bucket = args.bucket
+        else:
+             print("Error:  No bucket specified")
+             sys.exit(8)
         return days_specifed, file_prefix, my_bucket, dry_run
     #
     days_specifed, file_prefix, my_bucket, dry_run = check_args()
@@ -41,7 +52,7 @@ def app_run():
     retention_period = today - timedelta(days=days_specifed)
 
     del_list, found_list = process_s3_bucket(days_specifed, file_prefix, my_bucket,
-                      dry_run, today, retention_period)
+                                             dry_run, today, retention_period)
     return
 
 
@@ -55,6 +66,16 @@ def process_s3_bucket(days_specifed, file_prefix, my_bucket, dry_run, today, ret
     def s3_get_objects(s3, days_specifed, file_prefix, my_bucket, retention_period, delete_candidate_list):
         s3 = boto3.client('s3')
         list_found = []
+        try:
+            objects = s3.list_objects_v2(Bucket=my_bucket, Prefix=file_prefix)
+        except botocore.exceptions.ParamValidationError as e:
+            print("S3 Error: check bucket name and prefix: ",
+                  my_bucket, ", prefix =  ", file_prefix)
+            sys.exit(8)
+        except botocore.exceptions.ClientError as e:
+            print("Unexpected error: %s" % e)
+            sys.exit(8)
+
         objects = s3.list_objects_v2(Bucket=my_bucket, Prefix=file_prefix)
 
         for o in objects["Contents"]:
@@ -66,25 +87,25 @@ def process_s3_bucket(days_specifed, file_prefix, my_bucket, dry_run, today, ret
         print("***************Summary***************")
         print("Num of objects found:  ", len(objects["Contents"]))
         return list_found
-    
+
     def delete_summary(delete_candidate_list):
         if (len(delete_candidate_list) > 0):
             print("Number of files to delete: ", len(delete_candidate_list))
             print("deleting older files")
+
     def process_deletes(my_bucket, dry_run, delete_candidate_list, s3):
         for obj in delete_candidate_list:
             print("Deleting:  ", obj)
             if (dry_run == False):
                 s3.delete_object(Bucket=my_bucket, Key=obj)
-    
 
     delete_candidate_list = []
     found_candidate_list = []
     s3 = boto3.client('s3')
     s3_summary(file_prefix, my_bucket, today, retention_period)
     found_candidate_list = s3_get_objects(s3, days_specifed, file_prefix, my_bucket,
-                        retention_period, delete_candidate_list)
-    delete_summary(delete_candidate_list) 
+                                          retention_period, delete_candidate_list)
+    delete_summary(delete_candidate_list)
     process_deletes(my_bucket, dry_run, delete_candidate_list, s3)
     return delete_candidate_list, found_candidate_list
 
