3c515807bdcc5b72e592196652109a5ade06002a
Jon Allen <jallen_swrx@yahoo.com>
2021-08-10 17:07:06 -0400

    added min entries  to prevent deleting backups past a certain count  even if days have passed. ( failsafe )  On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   aws_s3_backup_retention.py

--- aws_s3_backup_retention.py
+++ aws_s3_backup_retention.py
@@ -21,6 +21,7 @@ def app_run():
         '--backup_prefix', help="daily backup file prefix - used\'myback\' not \'myback*\'")
     parser.add_argument('--dry_run', action="store_true",
                         help='dry-run for testing')
+    parser.add_argument('--min', help="lower min threshold")
     args = parser.parse_args()
     # special arg processing if necessary
 
@@ -35,28 +36,33 @@ def app_run():
             days_specifed = int(args.days)
         else:
             days_specifed = 10
-        if ( args.backup_prefix):
+        if (args.min):
+            min_entries = int(args.min)
+        else:
+            min_entries = 1
+        if (args.backup_prefix):
             file_prefix = args.backup_prefix
         else:
-             print("Error: No prefix specified")
-             sys.exit(8)
-        if ( args.bucket):
-              my_bucket = args.bucket
+            print("Error: No prefix specified")
+            sys.exit(8)
+        if (args.bucket):
+            my_bucket = args.bucket
         else:
-             print("Error:  No bucket specified")
-             sys.exit(8)
-        return days_specifed, file_prefix, my_bucket, dry_run
+            print("Error:  No bucket specified")
+            sys.exit(8)
+        return days_specifed, file_prefix, my_bucket, dry_run, min_entries
     #
-    days_specifed, file_prefix, my_bucket, dry_run = check_args()
+    days_specifed, file_prefix, my_bucket, dry_run, min_entries = check_args()
     today = datetime.now(timezone.utc)
     retention_period = today - timedelta(days=days_specifed)
 
     del_list, found_list = process_s3_bucket(days_specifed, file_prefix, my_bucket,
-                                             dry_run, today, retention_period)
+                                             dry_run, today, retention_period,
+                                             min_entries)
     return
 
 
-def process_s3_bucket(days_specifed, file_prefix, my_bucket, dry_run, today, retention_period):
+def process_s3_bucket(days_specifed, file_prefix, my_bucket, dry_run, today, retention_period, min_entries):
     def s3_summary(file_prefix, my_bucket, today, retention_period):
         print("today's date is ", today)
         print("Start of retention period (days) ", retention_period)
@@ -99,12 +105,21 @@ def process_s3_bucket(days_specifed, file_prefix, my_bucket, dry_run, today, ret
             if (dry_run == False):
                 s3.delete_object(Bucket=my_bucket, Key=obj)
 
+    def check_min_entries(min_entries, found_list):
+        print( "check_min function")
+        if (min_entries > len(found_list)):
+            print("min_entries:" + str(min_entries))
+            print("Number of items " + str(len(found_list)) +" less than min entries ")
+            print("**** exiting - check backup process - may not be running")
+            sys.exit(5)
+
     delete_candidate_list = []
     found_candidate_list = []
     s3 = boto3.client('s3')
     s3_summary(file_prefix, my_bucket, today, retention_period)
     found_candidate_list = s3_get_objects(s3, days_specifed, file_prefix, my_bucket,
                                           retention_period, delete_candidate_list)
+    check_min_entries(min_entries, found_candidate_list)
     delete_summary(delete_candidate_list)
     process_deletes(my_bucket, dry_run, delete_candidate_list, s3)
     return delete_candidate_list, found_candidate_list
