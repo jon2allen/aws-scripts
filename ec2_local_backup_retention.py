4cf62b4021da616102ba86a46689834591b52960
Jon Allen <jallen_swrx@yahoo.com>
2021-09-14 10:20:30 -0400

    added suffix support and minor cleanup

--- ec2_local_backup_retention.py
+++ ec2_local_backup_retention.py
@@ -17,7 +17,9 @@ def app_run():
     parser.add_argument('--days', help='days to retain ')
     parser.add_argument('--dir', help='Linux EC2 server dir')
     parser.add_argument(
-        '--backup_prefix', help="daily backup file prefix - used\'myback\' not \'myback*\'")
+        '--backup_prefix', help="daily backup file prefix - use \'myback\' not \'myback*\'")
+    parser.add_argument(
+        '--suffix', help="daily backup file suffix - use 'xls' not '*.xls'")
     parser.add_argument('--dry_run', action="store_true",
                         help='dry-run for testing')
     args = parser.parse_args()
@@ -35,10 +37,15 @@ def app_run():
         else:
             days_specifed = 10
         file_prefix = args.backup_prefix
+        file_suffix = args.suffix
+        if file_prefix is None:
+            file_prefix = " "
+        if file_suffix is None:
+            file_suffix = "... not specified"
         my_dir = args.dir
-        return days_specifed, file_prefix, my_dir, dry_run
+        return days_specifed, file_prefix, my_dir, dry_run, file_suffix
     #
-    days_specifed, file_prefix, my_dir, dry_run = check_args()
+    days_specifed, file_prefix, my_dir, dry_run, file_suffix = check_args()
     if my_dir == None:
         print("No dir specified - see -h for commands")
         sys.exit(4)
@@ -47,19 +54,20 @@ def app_run():
     retention_period = today - timedelta(days=days_specifed)
 
     # main_entry_point
-    process_ec2_dir(days_specifed, file_prefix, my_dir,
+    process_ec2_dir(days_specifed, file_prefix, file_suffix, my_dir,
                     dry_run, today, retention_period)
 
     return
 
 
-def process_ec2_dir(days_specifed, file_prefix, my_dir, dry_run, today, retention_period):
+def process_ec2_dir(days_specifed, file_prefix, suffix, my_dir, dry_run, today, retention_period):
 
-    def print_parms(file_prefix, my_dir, today, retention_period):
+    def print_parms(file_prefix, suffix, my_dir, today, retention_period):
         print("today's date is ", today)
         print("Start of retention period (days) ", retention_period)
         print("EC2 server dir:  ", my_dir)
-        print("backup prefix", file_prefix)
+        print("backup prefix: ", file_prefix)
+        print("backup suffix: ", suffix)
         return
 
     def delete_files(dry_run, delete_candidate_list):
@@ -85,7 +93,7 @@ def process_ec2_dir(days_specifed, file_prefix, my_dir, dry_run, today, retentio
         o_time = utc.localize(o_time)
         return o_time
 
-    def filter_dir_obj(days_specifed, file_prefix, my_dir, retention_period, filter_lists):
+    def filter_dir_obj(days_specifed, file_prefix, suffix, my_dir, retention_period, filter_lists):
         found_candidate_list = filter_lists[1]
         delete_candidate_list = filter_lists[0]
         objects = get_dir(my_dir)
@@ -93,12 +101,13 @@ def process_ec2_dir(days_specifed, file_prefix, my_dir, dry_run, today, retentio
         for o in objects:
             o_time = get_file_timestamp(utc, o)
             # print("file: ", o, "time: ", o_time )
-            if o.startswith(file_prefix):
-                print("file: ", o, "time: ", o_time)
+            if o.startswith(file_prefix) or (o.endswith(suffix)):
+                
                 found_candidate_list.append(o)
                 if o_time < retention_period:
-                    print("older than ", days_specifed)
+                    print("older than " , days_specifed, " ", end='')
                     delete_candidate_list.append(o)
+                print("file: ", o, "time: ", o_time)
         return
 
     def list_summary(found_candidate_list):
@@ -110,8 +119,8 @@ def process_ec2_dir(days_specifed, file_prefix, my_dir, dry_run, today, retentio
     found_candidate_list = []
     filter_lists = [delete_candidate_list, found_candidate_list]
     # main processing loop ec2 files
-    print_parms(file_prefix, my_dir, today, retention_period)
-    filter_dir_obj(days_specifed, file_prefix, my_dir,
+    print_parms(file_prefix, suffix, my_dir, today, retention_period)
+    filter_dir_obj(days_specifed, file_prefix, suffix, my_dir,
                    retention_period, filter_lists)
     list_summary(found_candidate_list)
     deletion_summary(delete_candidate_list)
